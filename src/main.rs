078af1031361c44d561e23d96cdd311c2cd25442
Kornel Lugosi <coornail@gmail.com>
2016-12-11 22:08:25 +0100

    Test analyze log file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use std::io::prelude::*;
+use std::io::{BufReader};
+use std::fs::File;
+use std::collections::HashMap;
+
+include!("./sentiment.rs");
+
 fn main() {
-    println!("Hello, world!");
+    let path = "/tmp/log".to_string();
+    let f = match File::open(&path) {
+        Err(why) => panic!("Couldn't open {}: {}", path, why),
+        Ok(f) => f,
+    };
+    let f = BufReader::new(f);
+
+    let mut result = HashMap::new();
+
+    let analizer = sentiment::new("/Users/coornail/rust/ircbot/src/wordlist.txt".to_string());
+
+    let mut i = 0;
+
+    for line in f.lines() {
+        // Parse lines.
+        let l = line.unwrap();
+        let parts = l.split("\t").collect::<Vec<&str>>();
+        let who = parts[1].to_string();
+
+        // Skip ignored names.
+        if ["*", "**", "***", "--", "---", "-->", "<--"].contains(&who.as_str()) {
+            continue;
+        }
+        i = i+1;
+
+        // Calculate new comment value.
+        let comment_value = analizer.analyze(parts[2]);
+        let sum_comment_value = match result.get(&who) {
+            Some(existing_comment_value) => existing_comment_value + comment_value,
+            None => comment_value,
+        };
+        result.insert(who, sum_comment_value);
+    }
+
+    print!("{:?}\n", result);
+    print!("{} lines analyzed.\n", i);
 }
